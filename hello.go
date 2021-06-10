19ac9e9f86da5878102fc2f109d78f42a10e9784
Carlos <albornozmcea@gmail.com>
2021-06-10 00:42:26 +0000

    Created more tests and refactored

--- hello.go
+++ hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const enGreetingPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return enGreetingPrefix + name
 }
 
 func main() {
--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Carlos")
-	want := "Hello, Carlos"
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+	t.Run("Say hello to people", func(t *testing.T) {
+		got := Hello("Carlos")
+		want := "Hello, Carlos"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("Say 'Hello, World' when a name is not passed", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }

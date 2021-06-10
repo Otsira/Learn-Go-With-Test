ac8dc132dc6fcce50dc663f83a25c19e331452cd
Carlos <albornozmcea@gmail.com>
2021-06-10 01:14:34 +0000

    Added more languages and refactored!

--- hello.go
+++ hello.go
@@ -2,15 +2,24 @@ package main
 
 import "fmt"
 
-const enGreetingPrefix = "Hello, "
+var greetings = map[string]string{
+	"en": "Hello, ",
+	"es": "Hola, ",
+	"fr": "Bonjour, ",
+}
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return enGreetingPrefix + name
+	// Quirks of go, but seems nice
+	//https://stackoverflow.com/questions/2050391/how-to-check-if-a-map-contains-a-key-in-go
+	if greet, ok := greetings[language]; ok {
+		return greet + name
+	}
+	return greetings["en"] + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -10,13 +10,23 @@ func TestHello(t *testing.T) {
 		}
 	}
 	t.Run("Say hello to people", func(t *testing.T) {
-		got := Hello("Carlos")
+		got := Hello("Carlos", "")
 		want := "Hello, Carlos"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("Say 'Hello, World' when a name is not passed", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("now in Spanish", func(t *testing.T) {
+		got := Hello("Carlos", "es")
+		want := "Hola, Carlos"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("French it's!", func(t *testing.T) {
+		got := Hello("Caloua", "fr")
+		want := "Bonjour, Caloua"
 		assertCorrectMessage(t, got, want)
 	})
 }
